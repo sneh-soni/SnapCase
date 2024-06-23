d7fdc7195355f0fc3e717ab97b77ecd9885cb81f
sneh-soni <ssneh20062003@gmail.com>
2024-06-23 18:53:04 +0530

    hero section Phone component (your-image)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Phone from "@/components/Phone";
 import MaxWidthWrapper from "@/components/maxWidthWrapper";
 import { Check, Star } from "lucide-react";
 
@@ -78,6 +79,19 @@ export default function Home() {
                   </p>
                 </div>
               </div>
+            </div>
+          </div>
+          <div className="col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit">
+            <div className="relative md:max-w-xl">
+              <img
+                src="/your-image.png"
+                className="absolute w-40 lg:w-52 left-52 -top-16 select-none hidden sm:block lg:hidden xl:block"
+              />
+              <img
+                src="/line.png"
+                className="absolute w-20 -left-6 -bottom-6 select-none"
+              />
+              <Phone className="w-64" imgSrc="/testimonial-1.jpg" />
             </div>
           </div>
         </MaxWidthWrapper>
